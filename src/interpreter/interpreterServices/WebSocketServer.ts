960713d21311471ecff0b21b31863c64b412baaf
vlasival <vlasival@gmail.com>
2024-12-14 21:42:05 +0700

    cors

--- src/interpreter/interpreterServices/WebSocketServer.ts
+++ src/interpreter/interpreterServices/WebSocketServer.ts
@@ -5,7 +5,12 @@ import { WebSocketService } from "./WebSocketService";
 export function startWebSocketServer(port: string | number): void {
     const httpServer = createServer();
 
-    const io = new Server(httpServer);
+    const io = new Server(httpServer, {
+        cors: {
+          origin: "http://localhost:3001",
+          methods: ["GET", "POST"],
+        },
+      });
 
     const webSocketService = new WebSocketService(io);
     webSocketService.start();
